3fe5f55bea988798ac88fc2c7408822bd6eb7fbc
nietras <nietras@users.noreply.github.com>
2026-02-28 13:37:56 +0100

    Initial

---
+++ disasm-tester.ps1
@@ -0,0 +1,5 @@
+﻿param(
+    [string]$runtime = "win-arm64"
+)
+dotnet publish src/CudaSharp.Tester/CudaSharp.Tester.csproj -c Release -r "$runtime" -f net10.0 --self-contained true /p:PublishAot=true /p:DebugSymbols=true
+dumpbin /DISASM /SYMBOLS "artifacts\publish\CudaSharp.Tester\release_$runtime\CudaSharp.Tester.exe" > "artifacts\publish\CudaSharp.Tester\release_$runtime\disassembly.asm"
