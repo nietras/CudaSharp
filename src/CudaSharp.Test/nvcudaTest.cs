dfc6570271afa93ca4fc8d1cfeb454ee3253e236
nietras <nietras@users.noreply.github.com>
2026-03-14 13:24:57 +0100

    skip tests if no init

--- src/CudaSharp.Test/nvcudaTest.cs
+++ src/CudaSharp.Test/nvcudaTest.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CudaSharp.Test;
 
 [TestClass]
 public class nvcudaTest
 {
+    public nvcudaTest()
+    {
+        try
+        {
+            nvcuda.cuInit(0);
+        }
+        catch (Exception ex)
+        {
+            Assert.Inconclusive($"CUDA initialization failed: {ex.Message}");
+        }
+    }
+
     [TestMethod]
     public void nvcudaTest_cuInit()
     {
