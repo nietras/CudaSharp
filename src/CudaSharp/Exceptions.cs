24cc5b53308caa816b785b83d1e5c0d397fa6de1
nietras <nietras@users.noreply.github.com>
2026-03-16 15:53:40 +0100

    add ToStringFast

---
+++ src/CudaSharp/Exceptions.cs
@@ -0,0 +1,11 @@
+﻿namespace CudaSharp;
+
+public class CudaException(string message) : Exception(message)
+{
+}
+
+public sealed class CudaException<TResult>(TResult result, string message) : Exception(message)
+    where TResult : unmanaged, Enum
+{
+    public TResult Result { get; } = result;
+}
